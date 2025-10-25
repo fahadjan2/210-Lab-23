3d1ca54165bafa2a013bdaeaf7ca2535b29fa8c6
fahadjan2 <fahadfawad2006@gmail.com>
2025-10-25 00:15:38 -0700

    Commit

---
+++ main.cpp
@@ -0,0 +1,55 @@
+// COMSC-210 | Lab 23 | Fahad Fawad Ahmad 
+// IDE used: Visual Studio
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <list>
+#include "Goat.h"
+using namespace std;
+
+const int SZ_NAMES = 200, SZ_COLORS = 25, MAX_AGE = 20;
+
+int select_goat(list<Goat> trip);
+void delete_goat(list<Goat> &trip);
+void add_goat(list<Goat> &trip, string [], string []);
+void display_trip(list<Goat> trip);
+int main_menu();
+
+int main() {
+    srand(time(0));
+    bool again;
+
+    // read & populate arrays for names and colors
+    ifstream fin("names.txt");
+    string names[SZ_NAMES];
+    int i = 0;
+    while (fin >> names[i++]);
+    fin.close();
+    ifstream fin1("colors.txt");
+    string colors[SZ_COLORS];
+    i = 0;
+    while (fin1 >> colors[i++]);
+    fin1.close();
+
+
+
+
+    return 0;
+}
+
+
+int main_menu() {
+    int choice;
+    cout << "*** GOAT MANAGER 3001 ***" << endl;
+    cout << "[1] Add a goat\n[2] Delete a goat\n[3] List goats\n[4] Quit" << endl;
+    cout << "Choice --> ";
+    cin >> choice;
+
+    while (choice != 1 && choice != 2 && choice != 3 && choice != 4) {
+        cout << "Choice must be within 1-4" << endl;
+        cout << "Choice --> ";
+        cin >> choice;
+    }
+
+    return choice;
+}
